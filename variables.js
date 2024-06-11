32208b741be6f9a8dba84b5c1dc5556222322efb
noscopeFanEX <starnesx@gmail.com>
2024-06-11 10:58:10 -0400

    code3.1

--- variables.js
+++ variables.js
@@ -2,17 +2,22 @@
 let favoriteColor;
 
 // 1. Create a variable called `favoriteFood`.
-
+let favoriteFood;
 
 // 2. Create a variable called `favoriteNumber`.
-
+let favoriteNumber;
 
 
 
 // CODE ALONG, PART 2
 //  3. Assign values for each variables.
 // - Use a string value of your favorite color.
+favoriteColor = "black";
 // - Use a string value of your favorite food.
+favoriteFood = "pizza";
 // - Use a number value of your favorite number.
-
+favoriteNumber = 64;
 // 4. Console log each variable on a new line.
+console.log(favoriteNumber);
+console.log(favoriteColor);
+console.log(favoriteFood);
